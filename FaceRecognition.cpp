3747a892dd323a98e3c02ff97970d44f1eb44955
andyrudovv <110022906+andyrudovv@users.noreply.github.com>
2024-04-30 13:55:56 +0300

    Well done

--- FaceRecognition.cpp
+++
@@ -1,1 +0,0 @@
-#include "FaceRecognition.h"
--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,37 @@
+#include <iostream>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/objdetect.hpp>
+
+using namespace cv;
 
 int main() {
+
+	VideoCapture video_capture(0);
+	Mat frame;
+
+	CascadeClassifier facedetect;
+
+	facedetect.load("Cascades/haarcascade_frontalface_default.xml");
+
+	std::vector<Rect> faces;
+
+
+	while (true) {
+
+		video_capture.read(frame);
+
+		facedetect.detectMultiScale(frame, faces, 1.3, 5);
+
+		for (int i = 0; i < faces.size(); i++) {
+			rectangle(frame, faces[i].tl(), faces[i].br(), Scalar(50,50,255), 3);
+		}
+
+		
+		imshow("Image", frame);
+		waitKey(1);
+	}
 
 	return 0;
 }
